ef1deebb3a69626ad19056dd57fd8842f1728f10
dimittriskalp <dimitriskalp@hotmail.gr>
2021-03-16 19:58:31 +0200

    the last updates

--- github.js
+++ github.js
@@ -2,14 +2,19 @@ class GitHub {
     constructor(){
         this.client_id = 'ead3ce22584269643c29';
         this.client_secret = '7f8deb586c97771f048bfd3e8d3713fe6ce62759';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
     }
     async getUser(user){
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }
--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI{
         this.profile = document.getElementById('profile');
     }
 
+    //display profile in ui
     showProfile(user){
         this.profile.innerHTML = `
          <div class="card card-body mb-3">
@@ -14,10 +15,10 @@ class UI{
 
              </div>
              <div class="col-md-9">
-               <span class="badge badge-primary">Public Repos: ${user.public_repos}</span 
-               <span class="badge badge-Secondary">Public Gists: ${user.public_gists}</span 
-               <span class="badge badge-success">Followers: ${user.followers}</span 
-               <span class="badge badge-info">Following: ${user.following}</span 
+               <span class="badge badge-primary">Public Repos: ${user.public_repos}</span> 
+               <span class="badge badge-secondary">Public Gists: ${user.public_gists}</span> 
+               <span class="badge badge-success">Followers: ${user.followers}</span> 
+               <span class="badge badge-info">Following: ${user.following}</span> 
                <br><br>
 
                <ul class="list-group-item">
@@ -33,4 +34,68 @@ class UI{
          <div id="repos"></div>
         `;
     }
+    //Show user repos
+    showRepos(repos){
+      let output = '';
+
+      repos.forEach(function(repo){
+        output +=`
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span> 
+              <span class="badge badge-secondary">Watchers: ${repo.watchers}</span> 
+              <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+            
+            </div>
+          </div>
+        </div>        
+        `;
+
+        //output repos
+        document.getElementById('repos').innerHTML = output;
+
+
+      });
+    }
+
+    //show alert message
+    showAlert(message, className){
+      //clear any remain alert
+      this.clearAlert();
+      //create div
+      const div = document.createElement('div');
+      //add classname
+      div.className = className;
+      //add text
+      div.appendChild(document.createTextNode(message));
+      //get parent
+      const container = document.querySelector('.searchContainer');
+      //search box
+      const search = document.querySelector('.search');
+      //insert alert
+      container.insertBefore(div,search);
+
+      //timeout after 3 sec
+      setTimeout(() =>{
+        this.clearAlert();
+      },3000);
+    }
+
+    clearAlert(){
+      const currentAlert = document.querySelector('.alert');
+
+      if(currentAlert){
+        currentAlert.remove();
+      }
+    }
+
+    clearProfile(){
+      this.profile.innerHTML = '';
+    }
+
 }
